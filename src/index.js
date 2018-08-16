22ae9d23e6eb23eabcfa476e154e0e015fc494ed
Eric Stout <estout@elmstreettechnology.com>
2018-08-16 15:49:12 -0600

    Add index to pull in all functions, inital flow setup

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const getBoxScore = require("./getBoxscore");
+const getColorFeed = require("./getColorFeed");
+const getGameFeed = require("./getGameFeed");
+const getGames = require("./getGames");
+const getLinescore = require("./getLinescore");
+const getStandings = require("./getStandings");
+const getTeams = require("./getTeams");
+
+module.exports = {
+  getBoxScore,
+  getColorFeed,
+  getGameFeed,
+  getGames,
+  getLinescore,
+  getStandings,
+  getTeams
+};
